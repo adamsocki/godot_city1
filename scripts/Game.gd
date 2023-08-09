593ca6cff2f06459b01021035329c87104bb6c46
adamsocki <socki.adam@gmail.com>
2023-08-09 14:05:15 -0500

    init MapLoad

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,15 +1,29 @@
 extends Node2D
 
-
+var tile_map_path := "res://TileMap/"
+enum {TILE_GROUND = 0, TILE_PLAYER = 1}
+var NodeTileMap
 
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Global.game = self
+	
+	
+	MapLoad()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+func MapLoad():
+	var next_level = min(Global.level, Global.last_level)
+	var tile_map = load(tile_map_path + str(next_level) + ".tscn").instantiate()
+	tile_map.name = "TileMap"
+	add_child(tile_map)
+	NodeTileMap = tile_map
--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,6 +1,11 @@
 extends Node
 
 
+var level := 0
+var last_level := 20
+var game
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
