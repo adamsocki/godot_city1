87a8e1ea957c0a82ee02bd29fff7e0a87e1b9bce
Adam Socki <socki.adam@gmail.com>
2023-08-16 17:07:50 -0500

    scale to map

--- Subway/grid_manager.gd
+++ Subway/grid_manager.gd
@@ -5,28 +5,24 @@ var Grid_Display_Scene
 
 # Called when the node enters the scene tree for the first time.
 func init_grid_manager():
-#	Global.grid = Grid.instantiate(10,10)
-#	Global.grid = Grid
+	
 	Global.grid = Grid.new(100,100)
 	var grid_display_scene = load("res://scenes/Grid_Display.tscn").instantiate()
 	grid_display_scene.name = "GridDisplay"
 	add_child(grid_display_scene)
 	Grid_Display_Scene = grid_display_scene
-
-#	stations.resize(3)
-	for a in 3:
+	
+	# CREATE RANDOM STATIONS
+	for a in 23:
 		var new_station = Global.grid.generate_new_station()
 #		stations.append(new_station)
 #		print(new_station.grid_location)
 	
 	Grid_Display_Scene.init_grid_display()
-	
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func update_grid_manager(delta):
-#	print(Global.grid.grid_size_x)
+	
 	Grid_Display_Scene.update_grid_display(delta)
 	
-	pass
--- scripts/Grid_display_script.gd
+++ scripts/Grid_display_script.gd
@@ -4,24 +4,49 @@ extends Node2D
 
 
 func init_grid_display():
+	
+#	var colorrect_size =  
+	$Map_Viewer.size = $Map_Viewer/Map_Size.get_size()
+	# LOAD AND SET STATIONS
 	for station in Global.grid.station_list:
-		print(station.grid_location)
+		
+		# SET STATION TO GRID
 		var station_load = load("res://scenes/station.tscn").instantiate()
 		station_load.set_station_data(station)
 		
-#		station_load.name = station.station_name
-#		station_load.grid_location = station.grid_location
-#		station_load.transform.position = station.grid_position
-		add_child(station_load)
-	
+		#SET STATION POSITIONS
+		var scaleFactor = Vector2($Map_Viewer.get_size().x / Global.grid.grid_size_x, $Map_Viewer.get_size().y / Global.grid.grid_size_y)
+		var scaledVector = Vector2(station.grid_location.x  * scaleFactor.x, station.grid_location.y * scaleFactor.y)
+
+#		var windowSize = Vector2($Map_Viewer.get_size().x, $Map_Viewer.get_size().y)
+		station_load.position = scaledVector
+#		station_load.position = station_load.position
+		
+		print(station.grid_location)
+		print(station_load.position)
+
+		
+		
+#		station_load.position.x = station.grid_location.x / $Map_Viewer.get_size().x
+#		station_load.position.y = station.grid_location.y / $Map_Viewer.get_size().y
+		
+#		station_load.get_child(0).size.x /= $Map_Viewer.get_size().x
+#		station_load.get_child(0).size.y /= $Map_Viewer.get_size().y		
+		
+		# ADD STATION AS CHILDREN
+		$Map_Viewer.add_child(station_load)
+#		print($Map_Viewer.get_size())
+
 
 
 func update_grid_display(delta):
-	var children = get_children()
-	for child in children:
-		var station_data = child.get_station_data()
-		child.position.x = station_data.grid_location.x * 4
-		child.position.y = station_data.grid_location.y * 4
-		
-		print("cs")
 	pass
+	
+#	var children = get_children()
+#	for child in children:
+#		var station_data = child.get_station_data()
+##		child.position.x = station_data.grid_location.x * 4
+##		child.position.y = station_data.grid_location.y * 4
+		
+#		print("cs")
+	
